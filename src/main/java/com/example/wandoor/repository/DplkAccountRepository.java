7f7e58f59b812e85dc2e452f2387a85493f09899
Oktavia <oktaviaq.a@gmail.com>
2025-10-22 23:23:04 +0700

    add fitur get list dplk items

--- src/main/java/com/example/wandoor/repository/DplkAccountRepository.java
+++ src/main/java/com/example/wandoor/repository/DplkAccountRepository.java
@@ -1,4 +1,10 @@
 package com.example.wandoor.repository;
 
-public interface DplkAccountRepository {
+import com.example.wandoor.model.entity.DplkAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DplkAccountRepository extends JpaRepository<DplkAccount, String> {
+    List<DplkAccount> findAllByUserIdAndCif(String userId, String cif);
 }
