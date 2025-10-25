c0ddfe4a8e08556f7086fdc68d6227bac17ea4d2
Erlangga Wahyu Utomo <erlanggawahyu21@gmail.com>
2025-10-26 03:30:54 +0700

    adding fix account feature

--- src/main/java/com/example/wandoor/controller/SplitBillController.java
+++ src/main/java/com/example/wandoor/controller/SplitBillController.java
@@ -1,56 +1,56 @@
-package com.example.wandoor.controller;
+// package com.example.wandoor.controller;
 
-import com.example.wandoor.model.request.SplitBillDetailRequest;
-import com.example.wandoor.model.response.SplitBillDetailResponse;
-import com.example.wandoor.model.response.SplitBillsListResponse;
-import com.example.wandoor.service.SplitBillService;
+// import com.example.wandoor.model.request.SplitBillDetailRequest;
+// import com.example.wandoor.model.response.SplitBillDetailResponse;
+// import com.example.wandoor.model.response.SplitBillsListResponse;
+// import com.example.wandoor.service.SplitBillService;
 
-import com.example.wandoor.model.request.AddNewSplitBillRequest;
-import com.example.wandoor.model.response.SplitBillsListResponse;
-import com.example.wandoor.service.SplitBillService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
+// import com.example.wandoor.model.request.AddNewSplitBillRequest;
+// import jakarta.validation.Valid;
+// import lombok.extern.log4j.Log4j2;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-import java.util.Map;
+// import java.net.URI;
+// import java.util.Map;
+// import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequestMapping("api/split-bill")
-@RequiredArgsConstructor
-@Log4j2
-public class SplitBillController {
-    private final SplitBillService splitBillService;
+// @RestController
+// @RequestMapping("api/split-bill")
+// @RequiredArgsConstructor
+// @Log4j2
+// public class SplitBillController {
+//     private final SplitBillService splitBillService;
 
-    @GetMapping
-    public ResponseEntity<SplitBillsListResponse> getAllSplitBills(){
-        SplitBillsListResponse response = splitBillService.getAllSplitBill();
-        return ResponseEntity.ok(response);
-    }
+//     @GetMapping
+//     public ResponseEntity<SplitBillsListResponse> getAllSplitBills(){
+//         SplitBillsListResponse response = splitBillService.getAllSplitBill();
+//         return ResponseEntity.ok(response);
+//     }
 
-    @PostMapping("/detail")
-    public ResponseEntity<SplitBillDetailResponse> getAllSplitBillMember(@Valid @RequestBody SplitBillDetailRequest request){
-        SplitBillDetailResponse response = splitBillService.getAllSplitBillMember(request);
-        return ResponseEntity.ok(response);
-    @PostMapping("/add")
-    public ResponseEntity<Map<String, String>> createSplitBill(
-            @Valid @RequestBody AddNewSplitBillRequest request){
+//     @PostMapping("/detail")
+//     public ResponseEntity<SplitBillDetailResponse> getAllSplitBillMember(@Valid @RequestBody SplitBillDetailRequest request){
+//         SplitBillDetailResponse response = splitBillService.getAllSplitBillMember(request);
+//         return ResponseEntity.ok(response);
+//     @PostMapping("/add")
+//     public ResponseEntity<Map<String, String>> createSplitBill(
+//             @Valid 
+//             @RequestBody 
+//             AddNewSplitBillRequest request){
 
-        log.info("Receive create split bill request: {}", request.splitBillTitle());
-        String splitBillId = splitBillService.createSplitBill(request);
+//         log.info("Receive create split bill request: {}", request.splitBillTitle());
+//         String splitBillId = splitBillService.createSplitBill(request);
 
-        return ResponseEntity
-                .created(URI.create("api/split-bill" + splitBillId))
-                .body(Map.of(
-                        "splitBillId", splitBillId,
-                        "message", "Split bill created successfully"
-                ));
-    }
-}
+//         return ResponseEntity
+//                 .created(URI.create("api/split-bill" + splitBillId))
+//                 .body(Map.of(
+//                         "splitBillId", splitBillId,
+//                         "message", "Split bill created successfully"
+//                 ));
+//     }
+// }
