2fa2e376cccd7dffc4142309226d381480b23f66
Oktavia <oktaviaq.a@gmail.com>
2025-10-30 11:07:56 +0700

    FIXING UNIT TESTTTTTTTTTTTTTTTTTTTTTTTT

--- src/test/java/com/example/wandoor/service/LoginOtpServiceTest.java
+++ src/test/java/com/example/wandoor/service/LoginOtpServiceTest.java
@@ -9,15 +9,21 @@ import com.example.wandoor.model.request.VerifyOtpRequest;
 import com.example.wandoor.repository.*;
 import com.example.wandoor.service.EmailService;
 import com.example.wandoor.util.JwtUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -41,6 +47,14 @@ class LoginOtpServiceTest {
     private JwtUtils jwtUtils;
     @Mock
     private StringRedisTemplate stringRedisTemplate;
+    @Mock
+    private RedisTemplate<String, Object> redisTemplate;
+    @Mock
+    private HashOperations<String, String, String> hashOperations;
+    @Mock
+    private ValueOperations<String, String> valueOperations;
+
+
 
     @InjectMocks
     private LoginOtpService loginOtpService;
@@ -50,7 +64,21 @@ class LoginOtpServiceTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        when(stringRedisTemplate.opsForHash()).thenReturn((HashOperations) hashOperations);
+        when(stringRedisTemplate.opsForValue()).thenReturn(valueOperations);
+        when(stringRedisTemplate.hasKey(anyString())).thenReturn(false);
+        when(valueOperations.increment(anyString())).thenReturn(1L);
+        when(stringRedisTemplate.expire(anyString(), any())).thenReturn(true);
+        when(stringRedisTemplate.delete(anyString())).thenReturn(true);
+        doNothing().when(hashOperations).putAll(anyString(), anyMap());
+
     }
+
+    @AfterEach
+    void cleanRedis() {
+        stringRedisTemplate.delete("otp:*");
+    }
+
 
     @Test
     void testLogin_success() {
@@ -70,12 +98,23 @@ class LoginOtpServiceTest {
         profile.setEmailAddress("oktaviaqa@example.com");
         when(profileRepository.findById("U001")).thenReturn(Optional.of(profile));
 
-        when(userOtpVerificationRepository.save(any(OtpVerification.class)))
-                .thenAnswer(inv -> {
-                    OtpVerification otp = inv.getArgument(0);
-                    otp.setId(UUID.randomUUID().toString());
-                    return otp;
-                });
+        when(stringRedisTemplate.hasKey(anyString())).thenReturn(false);
+        when(stringRedisTemplate.opsForValue()).thenReturn(valueOperations);
+        when(stringRedisTemplate.opsForHash()).thenReturn((HashOperations) hashOperations);
+        when(valueOperations.increment(anyString())).thenReturn(1L);
+        when(stringRedisTemplate.expire(anyString(), any())).thenReturn(true);
+        when(stringRedisTemplate.delete(anyString())).thenReturn(true);
+
+        doNothing().when(hashOperations).putAll(anyString(), anyMap());
+
+        doNothing().when(emailService).sendOtp(anyString(), anyString());
+
+//        when(userOtpVerificationRepository.save(any(OtpVerification.class)))
+//                .thenAnswer(inv -> {
+//                    OtpVerification otp = inv.getArgument(0);
+//                    otp.setId(UUID.randomUUID().toString());
+//                    return otp;
+//                });
 
         var response = loginOtpService.login(req);
 
@@ -94,8 +133,14 @@ class LoginOtpServiceTest {
         userAuth.setUserId("U001");
         userAuth.setUsername("oktaviaqa");
         userAuth.setPassword(encoder.encode("123456"));
+        userAuth.setIsUserBlocked(0);
 
         when(userAuthRepository.findByUsername("oktaviaqa")).thenReturn(Optional.of(userAuth));
+
+        when(stringRedisTemplate.hasKey(anyString())).thenReturn(false);
+        when(stringRedisTemplate.opsForValue()).thenReturn(valueOperations);
+        when(valueOperations.increment(anyString())).thenReturn(1L);
+        when(stringRedisTemplate.expire(anyString(), any())).thenReturn(true);
 
         var ex = catchThrowable(() -> loginOtpService.login(req));
 
@@ -135,12 +180,29 @@ class LoginOtpServiceTest {
                 .isUsed(0)
                 .expiresAt(LocalDateTime.now().plusMinutes(2))
                 .build();
+        var profile = new Profile();
+        profile.setId("U001");
+        profile.setEmailAddress("oktaviaqa@example.com");
+
+        String otpSessionKey = "otp:session:" + otpId;
+        Map<Object, Object> mockOtpData = new HashMap<>();
+        mockOtpData.put("otp", "654321");
+        mockOtpData.put("username", "oktaviaqa");
+        mockOtpData.put("userId", "U001");
+
 
         when(userOtpVerificationRepository.consumeIfValid(anyString(), anyString(), any()))
                 .thenReturn(1);
         when(userOtpVerificationRepository.findById(anyString())).thenReturn(Optional.of(otpVerification));
-        when(userAuthRepository.findById("U001")).thenReturn(Optional.of(new UserAuth("U001", "oktaviaqa", null, null, null, 0)));
+        when(userAuthRepository.findById(eq("U001"))).thenReturn(Optional.of(new UserAuth("U001", "oktaviaqa", null, null, null, 0)));
+        when(profileRepository.findById("U001")).thenReturn(Optional.of(profile));
         when(jwtUtils.generateToken(anyString(), anyString())).thenReturn("jwt_token");
+
+        when(stringRedisTemplate.opsForHash()).thenReturn((HashOperations) hashOperations);
+        when(stringRedisTemplate.opsForValue()).thenReturn(valueOperations);
+        when(hashOperations.entries(eq(otpSessionKey))).thenReturn((Map) mockOtpData);
+        when(valueOperations.increment(anyString())).thenReturn(1L);
+        when(stringRedisTemplate.expire(anyString(), any())).thenReturn(true);
 
         var response = loginOtpService.verifyOtp(req);
 
@@ -155,17 +217,12 @@ class LoginOtpServiceTest {
         var otpId = UUID.randomUUID().toString();
         var req = new VerifyOtpRequest(otpId.toString(), "123456");
 
-        var otpVerification = OtpVerification.builder()
-                .id(otpId)
-                .otpCode("123456")
-                .isUsed(0)
-                .expiresAt(LocalDateTime.now().minusMinutes(1))
-                .build();
+        String otpSessionKey = "otp:session:" + otpId;
 
-        when(userOtpVerificationRepository.consumeIfValid(anyString(), anyString(), any()))
-                .thenReturn(0);
-        when(userOtpVerificationRepository.findByIdAndIsUsed(anyString(), eq(0)))
-                .thenReturn(Optional.of(otpVerification));
+        when(stringRedisTemplate.opsForHash()).thenReturn((HashOperations) hashOperations);
+        when(hashOperations.entries(eq(otpSessionKey))).thenReturn(Map.of());
+
+        when(userAuthRepository.findById(eq("U001"))).thenReturn(Optional.of(new UserAuth("U001", "oktaviaqa", null, null, null, 0)));
 
         var ex = catchThrowable(() -> loginOtpService.verifyOtp(req));
 
